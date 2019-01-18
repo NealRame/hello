53f7ea80a72ffff2d1ce5caa6a3ffa5576adcf72
NealRame <contact@nealrame.com>
2019-01-18 21:05:54 +0100

    Import sources

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <hello.h>
+
+namespace nr {
+std::string hello() {
+  return "Hello, world!";
+}
+}
---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#include <hello.h>
+#include <iostream>
+
+int
+main(int argc, char **argv) {
+  std::cout << nr::hello() << std::endl;
+  return 0;
+}
